eb2a75963bd8173aa61f91abacdc09dca5cf6cdd
Marc GLOAGUEN <marc.gloaguen@gmail.com>
2024-03-07 10:30:55 +0100

    Changes to be committed: 	modified:   front.py add docstring

--- front.py
+++ front.py
@@ -4,6 +4,10 @@ from PIL import Image
 
 
 def main():
+    """
+    Main function of the Streamlit app that configures the page and calls
+    the function to display the PDF.
+    """
     st.set_page_config(
         page_title="PDF Extract",
         page_icon="📑",
@@ -19,6 +23,18 @@ def main():
 
 
 def image_from_page(doc: fitz.Document, n_page: int, bw: bool = True) -> Image:
+    """
+    Converts a PDF page to an image.
+
+    Args:
+        doc (fitz.Document): PDF document.
+        n_page (int): Number of the page to convert to an image.
+        bw (bool, optional): If True, the image will be in black and white.
+            Otherwise, it will be in color. Defaults to True.
+
+    Returns:
+        Image: Image of the PDF page.
+    """
     index_page = n_page-1
     page = doc.load_page(index_page)
     pix = page.get_pixmap()
@@ -29,6 +45,15 @@ def image_from_page(doc: fitz.Document, n_page: int, bw: bool = True) -> Image:
 
 
 def show_pdf(pdf_file, col1, col2, selected_pages):
+    """
+    Displays the uploaded PDF and allows the user to select pages.
+
+    Args:
+        pdf_file: Uploaded PDF file.
+        col1: First column of the Streamlit page.
+        col2: Second column of the Streamlit page.
+        selected_pages: Dictionary of selected pages.
+    """
     pdf_document = fitz.open(stream=pdf_file.read(), filetype="pdf")
     with col2.container(height=700, border=False):
         for page_nbr in range(1, 1+pdf_document.page_count):
