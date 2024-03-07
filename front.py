1d90e4917af49248a28fa61deb5bc89d957c279f
Marc GLOAGUEN <marc.gloaguen@gmail.com>
2024-03-07 15:48:33 +0100

    Changes to be committed: 	modified:   front.py

--- front.py
+++ front.py
@@ -1,6 +1,7 @@
 import streamlit as st
-import fitz
 from PIL import Image
+import PyPDF2
+import fitz
 
 
 def main():
@@ -15,70 +16,56 @@ def main():
         menu_items=None
         )
     st.title("PDF Extract")
-    infos, pdf = st.columns([1, 2])
+    infos, pdf = st.columns(2)
     pdf_file = infos.file_uploader("Instruct", type=["pdf"])
-    selected_pages = {}
+
     if pdf_file is not None:
-        show_pdf(pdf_file, infos, pdf, selected_pages)
+        show_pdf(pdf_file, infos, pdf)
 
 
-def img_from_page(doc: fitz.Document, n_page: int, col: bool = True) -> Image:
-    """
-    Converts a PDF page to an image.
-
-    Args:
-        doc (fitz.Document): PDF document.
-        n_page (int): Number of the page to convert to an image.
-        c (bool, optional): If True, the image will be in color.
-        Otherwise, it will be in balck and white.
-
-    Returns:
-        Image: Image of the PDF page.
-    """
-    index_page = n_page-1
-    page = doc.load_page(index_page)
+def img_from_page(doc: fitz.Document, n_page: int) -> Image:
+    page = doc.load_page(n_page-1)
     pix = page.get_pixmap()
     image = Image.frombytes("RGB", [pix.width,  pix.height], pix.samples)
-    if not col:
-        image = image.convert("L")
     return image
 
 
-def show_pdf(pdf_file, col1, col2, selected_pages: dict):
-    """
-    Displays the uploaded PDF and allows the user to select pages.
+def show_pdf(pdf_file, col1, col2):
+    pdf = fitz.open(stream=pdf_file.read(), filetype="pdf")
+    pdf_reader = PyPDF2.PdfReader(pdf_file)
+    nbr_pages = len(pdf_reader.pages)
+    page_num = col2.selectbox("Sélectionner une page", range(1, nbr_pages+1))
+    pdf_page = pdf_reader.pages[page_num-1]
 
-    Args:
-        pdf_file: Uploaded PDF file.
-        col1: First column of the Streamlit page.
-        col2: Second column of the Streamlit page.
-        selected_pages (dict): Dictionary of selected pages.
-    Returns:
-        Displays PDF and the page's checkbox
-    """
-    pdf_document = fitz.open(stream=pdf_file.read(), filetype="pdf")
-    number_pages = pdf_document.page_count
+    container1 = col1.container(height=600)
+    container1.image(img_from_page(pdf, page_num))
 
-    height = number_pages * 40 if number_pages < 10 else 400
-    check_container = col1.container(height=height, border=False)
-    pdf_container = col2.container(height=700, border=False)
-
-    for page_nbr in range(1, 1+number_pages):
-        selected = check_container.checkbox(f'Page : {page_nbr}', value=True)
-        if selected:
-            selected_pages[page_nbr] = True
-        else:
-            selected_pages.pop(page_nbr, None)
-
-        pdf_container.title(f'Page {page_nbr} :')
-        pdf_container.image(
-            img_from_page(pdf_document, page_nbr, selected),
-            use_column_width="always"
-            )
-
-    if col1.button('Upload'):
+    col2.text_area("Text to clean", pdf_page.extract_text(), 600)
+    if col2.button('Upload  clean_text'):
         st.toast('Pdf upload !')
-        print(selected_pages.keys())
+
+    st.markdown(
+        """
+        ---
+        ##### Instruction
+        Utilisez le caractère dièse (#) suivi d'un espace pour définir des titres. Par exemple :
+        ```
+        # Titre principal
+        ## Sous-titre
+        ### Sous-sous-titre
+        ```
+        Utilisez trois tirets (-) ou astérisques (*) sur une ligne pour créer un séparateur horizontal :
+        ```
+        ---
+        ```
+
+        ou
+
+        ```
+        ***
+        ```
+    """
+    )
 
 
 if __name__ == "__main__":
